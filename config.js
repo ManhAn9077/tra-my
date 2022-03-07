2b897daf6b2a394d3ee2acc8bc8d91c2e9b2179d
ManhAn9077 <94302143+ManhAn9077@users.noreply.github.com>
2022-03-07 21:57:54 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'My';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100015755700996';
+const eventName = 'Chúc Mừng 08/03';
+const titleCard = 'Tặng lớp trưởng';
+const contentCard = 'Chúc lớp trưởng tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với lớp trưởng trong hôm nay , cả những ngày sau và đỗ được trường cấp 3 mong muốn';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://scontent-sin6-4.xx.fbcdn.net/v/t39.30808-6/273278623_1170653273469852_5059609285034499957_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=wDcDmwMnSloAX_H8B5o&_nc_ht=scontent-sin6-4.xx&oh=00_AT-rWd6JLapDbVPTqBuPdMbrbK58CRv81j_uphOS5CZO0g&oe=622AD432';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
